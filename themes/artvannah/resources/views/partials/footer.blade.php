3438720baac184c58fa3a9ade4c3429dfe5d950b
Quentin Vannarath <qvannarath@gmail.com>
2025-08-20 18:36:22 +0200

    ✨ [FOOTER] Template - Styles - RWD

--- themes/artvannah/resources/views/partials/footer.blade.php
+++ themes/artvannah/resources/views/partials/footer.blade.php
@@ -2,17 +2,15 @@
 
 @include('elements/sprite')
 <footer class="footer">
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-auto">
-        <div class="footer__links">
-          @if (is_front_page())
-            <div class="footer__links-credit">
-              <a class="link" href="https://artvannah.fr/" target="_blank" rel="noopener">Création de site internet</a>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-24 col-md-22 m-auto">
+                <div class="footer__logo u-padding">
+                    <a href="{{ home_url('/') }}" aria-label="Retour à l'accueil" role="link" tabindex="0">
+                        {!! display_svg('logo') !!}
+                    </a>
+                </div>
             </div>
-          @endif
         </div>
-      </div>
     </div>
-  </div>
 </footer>
